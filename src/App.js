bb158282113b863c22959e94cb97ab7245dc8285
Mia5391 <mariakrasteva5391@gmail.com>
2020-10-04 15:06:13 -0300

    "initial app structure"

--- src/App.js
+++ src/App.js
@@ -8,15 +8,15 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          Te damos la bienvenida a Sonomed!
         </p>
         <a
           className="App-link"
-          href="https://reactjs.org"
+          href="/calendar"
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React
+          Reservar Hora
         </a>
       </header>
     </div>
